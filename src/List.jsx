56e52dd4e365a55191bc8959a3959cde2900bd17
Umar Farooque <80164102+Umarfarooque05@users.noreply.github.com>
2022-04-03 04:34:44 -0700

    Add files via upload

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Chocolate</li>
+      <li>Tea</li>
+      <li>Coffee</li>
+    </ul>
+  );
+}
+
+export default List;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Heading from "./Heading";
+import List from "./List";
+
+ReactDOM.render(
+    <App/>
+    ,
+  document.getElementById("root")
+);
